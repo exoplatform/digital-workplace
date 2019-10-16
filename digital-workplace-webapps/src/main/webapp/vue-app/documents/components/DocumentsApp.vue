d15ddf143d410cd38eb18a51a60c134564525201
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2019-10-16 12:09:08 +0100

    Implement tasks and documents portlets (#3)

--- digital-workplace-webapps/src/main/webapp/vue-app/documents/components/DocumentsApp.vue
+++ digital-workplace-webapps/src/main/webapp/vue-app/documents/components/DocumentsApp.vue
@@ -1,19 +1,50 @@
 <template>
   <v-app
-    class="mx-auto rounded-card"
+    class="mx-auto"
     color="transaprent">
-    <v-app-title class="ml-4">
-      <h3 class="color-title text-uppercase" background-color="white">Documents</h3>
-    </v-app-title>
+
+    <v-container class="white">
+      <v-flex
+        d-flex
+        xs12
+        px-3>
+        <v-layout
+          row
+          mx-0
+          align-center>
+          <v-flex
+            d-flex
+            xs12
+            sm6>
+            <v-card
+              flat
+              color="transparent">
+              <v-card-text class="body-1 text-uppercase grey--text px-0">
+                Documents
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-container>
     <v-tabs
       background-color="white"
-      color="#568DC9"
       right>
-      <v-tabs-slider 
-        color="#568DC9"/>
-      <v-tab class="ml-4 text-capitalize" >Recents</v-tab>
-      <v-tab class="ml-4 text-capitalize" >Shared with me</v-tab>
-      <v-tab class="ml-4 text-capitalize" >Favorites</v-tab>
+      <v-tabs-slider class="light-blue darken-3"/>
+      <v-layout>
+        <v-flex
+          row>
+          <v-flex>
+            <v-tab class="text-capitalize" >Recents</v-tab>
+          </v-flex>
+          <v-flex>
+            <v-tab class="text-capitalize" >Shared with me</v-tab>
+          </v-flex>
+          <v-flex>
+            <v-tab class="text-capitalize" >Favorites</v-tab>
+          </v-flex>
+        </v-flex>
+      </v-layout>
       <v-tab-item 
         v-for="n in 3"
         :key="n"
@@ -25,16 +56,17 @@
                 <v-list-item
                   v-for="(item, i) in items"
                   :key="i">
-                  <v-list-item-icon >
-                    <v-icon :color="item.iconColor" x-large>{{ item.icon }}</v-icon>
-                  </v-list-item-icon>
+                  <v-list-item-icon class="mr-0">
+                    <v-icon
+                      :color="item.iconColor"
+                      x-large>{{ item.icon }}</v-icon>
                   
-                  <v-list-item-content class="ml-4 mt-1">
-                    <v-list-item-title v-text="item.text"/>
-                    <v-list-item-subtitle>{{ item.date }}<v-icon>mdi-menu-right</v-icon>{{ item.space }}</v-list-item-subtitle>
-                  </v-list-item-content>
+                    <v-list-item-content class="ml-3 mt-1">
+                      <v-list-item-title v-text="item.text"/>
+                      <v-list-item-subtitle>{{ item.date }}<v-icon>mdi-menu-right</v-icon>{{ item.space }}</v-list-item-subtitle>
+                    </v-list-item-content>
                   
-                </v-list-item>
+                </v-list-item-icon></v-list-item>
               </v-list-item-group>
             </v-list>
           </v-row>
@@ -49,19 +81,19 @@
       items: [
         { text: 'Secondary line text Lorem ', iconColor:'#D07B7B',
           date:'22-05-2020',
-          space:'bla bla bla',icon: 'mdi-file-pdf' },
+          space:'UX Design & Prototypes',icon: 'mdi-file-pdf' },
         { text: 'Secondary line text Lorem',  iconColor:'#476A9C',
           date:'22-05-2020',
-          space:'bla bla bla',icon: 'mdi-file-word' },
+          space:'Product Management',icon: 'mdi-file-word' },
         { text: 'Secondary line text Lorem',  iconColor:'#368272',
           date:'22-05-2020',
-          space:'bla bla bla',icon: 'mdi-file-excel' },
+          space:'Product Management',icon: 'mdi-file-excel' },
         { text: 'Secondary line text Lorem',  iconColor:'#FBB340',
           date:'22-05-2020',
-          space:'bla bla bla',icon: 'mdi-image' },
+          space:'UX Design & Prototypes',icon: 'mdi-image' },
         { text: 'Secondary line text Lorem',  iconColor:'#368272',
           date:'22-05-2020',
-          space:'bla bla bla', icon: 'mdi-file-excel' },
+          space:'UX Design & Prototypes', icon: 'mdi-file-excel' },
 
       ],
     }),
