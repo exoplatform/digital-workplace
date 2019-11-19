8643d9508c3af0c702887f6923b8029d4db75794
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2019-11-19 12:31:42 +0100

    US-7.5: Update css of notifications list drawer (#75)

--- digital-workplace-webapps/src/main/webapp/vue-app/topBar/notification/components/NotificationApp.vue
+++ digital-workplace-webapps/src/main/webapp/vue-app/topBar/notification/components/NotificationApp.vue
@@ -1,7 +1,8 @@
 <template>
-  <v-layout>
-    <v-container style="width:25px" px-0>
-      <v-layout justify-center>
+  <v-app
+    class="VuetifyApp">
+    <v-container pa-0>
+      <v-layout class="white px-3 pt-3">
         <v-btn
           icon
           class="text-xs-center"
@@ -22,31 +23,76 @@
           right
           absolute
           temporary
-          width="350">
-          <v-card
-            flat
-            class="mx-auto">
-            <v-list three-line>
+          width="420">
+          <v-container pa-0>
+            <v-layout row mx-0>
+              <v-flex xs12 >
+                <v-card flat>
+                  <v-layout>
+                    <v-flex xs11>
+                    <v-card-text style="font-size: 17px;font-weight: bold">Notifications</v-card-text></v-flex>
+                    <v-flex xs1>
+                      <v-btn
+                        class="my-2 grey-color"
+                        icon
+                        @click.stop="drawer = !drawer"><v-icon size="30">mdi-close</v-icon></v-btn></v-flex></v-layout></v-card></v-flex>
+              <v-divider
+                :inset="inset"
+                class="mx-1 my-0"/>
+              <v-flex xs12 >
+                <v-card flat>
+                  <v-layout>
+                    <v-flex xs8>
+                      <v-btn
+                        style="border-radius: 1em;"
+                        depressed
+                        color="#E1E8EE"
+                        flat 
+                        class="my-2 ml-2"><span class="font-weight-black">See all</span></v-btn></v-flex>
+                    <v-flex xs4>
+                      <v-btn
+                        text
+                        class="my-2"><span class="primary-color--text">Mark all as read</span></v-btn></v-flex></v-layout></v-card></v-flex>
+              <v-divider
+                :inset="inset"
+                class="mx-1 my-0"/>
+              <v-flex xs12 >
+                <v-card
+                  flat
+                  class="mx-auto">
+                  <v-list three-line>
 
-              <v-list-item
-                v-for="(item, i) in items"
-                :key="i">
-                <v-card style="display: contents">
-                  <v-list-item-avatar style="padding-right:12px">
-                    <v-img :src="item.avatar"/>
-                  </v-list-item-avatar>
+                    <v-list-item
+                      v-for="(item, i) in items"
+                      :key="i">
+                      <v-layout :style="{background:item.background}">
+                        <v-flex xs2>
+                          <v-card style="display: contents">
+                            <v-list-item-avatar class="ml-4">
+                              <v-img :src="item.avatar"/>
+                            </v-list-item-avatar>
+                          </v-card>
+                        </v-flex>
+                        <v-flex xs9>
+                          <v-list-item-content>
+                            <v-list-item-title style="white-space: normal" v-html="item.title"/>
+                            <v-list-item-subtitle v-html="item.subtitle"/>
+                          </v-list-item-content>
+                        </v-flex>
+                        <v-flex xs1>
+                          <v-btn icon><v-icon small class="primary-color--text">mdi-close</v-icon></v-btn>
+                        </v-flex>
+                      </v-layout>
+                    </v-list-item>
+                  </v-list>
                 </v-card>
-                <v-list-item-content>
-                  <v-list-item-title style="white-space: normal" v-html="item.title"/>
-                  <v-list-item-subtitle v-html="item.subtitle"/>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list>
-          </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
         </v-navigation-drawer>
       </v-layout>
     </v-container>
-  </v-layout>
+  </v-app>
 </template>
 <script>
   export default {
@@ -56,13 +102,16 @@
         items: [
           {
             avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
-            title: "<span class='primary-color--text' >Sara</span>  has commented on your task in <span style='font-weight: bold'>Maintenance Board</span> project",
+            title: "<span class='primary-color--text' span style='font-weight: bold'>Sara</span>  has commented on your task in <span style='font-weight: bold'>Maintenance Board</span> project",
             subtitle: "3 hours ago",
+            background:"aliceblue",
           },
           {
             avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
-            title: "<span class='primary-color--text' >Hamadi</span>  has commented on your task in <span style='font-weight: bold'>Maintenance Board</span> project",
+            title: "<span class='primary-color--text' span style='font-weight: bold'>Hamadi</span>  has commented on your task in <span style='font-weight: bold'>Maintenance Board</span> project",
             subtitle: "1 day ago",
+            background:"",
+
           },
         ],
         mini: false,
