107248fac1daae98559c372e28da69cbf7fb7ed0
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2019-11-20 12:29:54 +0100

    US-7.1: Implement  top company navigation portlet (#81)

---
+++ digital-workplace-webapps/src/main/webapp/vue-app/topBar/topNavigation/components/TopNavigation.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app 
+    id="NavigationToolbarPortlet"
+    class="VuetifyApp">
+    <v-tabs color="#578DC9">
+      <v-tab class="px-0 pb-2" href="home">SNAPSHOT</v-tab>
+      <v-tab class="px-0 pb-2" href="#">STREAM</v-tab>
+      <v-tab class="px-0 pb-2" href="#">SPACES</v-tab>
+      <v-tab class="px-0 pb-2" href="#">PEOPLE</v-tab>
+    </v-tabs>
+  </v-app>
+</template>
