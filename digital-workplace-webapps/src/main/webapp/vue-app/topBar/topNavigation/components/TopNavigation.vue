58dbed496235c0cbba1d67394a86fa55ac89485f
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2019-11-21 11:03:04 +0100

    US-7.1.1: Display top company navigation with tabs (#86)

--- digital-workplace-webapps/src/main/webapp/vue-app/topBar/topNavigation/components/TopNavigation.vue
+++ digital-workplace-webapps/src/main/webapp/vue-app/topBar/topNavigation/components/TopNavigation.vue
@@ -1,12 +1,14 @@
 <template>
   <v-app 
     id="NavigationToolbarPortlet"
-    class="VuetifyApp">
-    <v-tabs color="#578DC9">
+    class="VuetifyApp"
+    style="max-width: 400px;">
+    <v-tabs center-active color="#578DC9">
       <v-tab class="px-0 pb-2" href="home">SNAPSHOT</v-tab>
       <v-tab class="px-0 pb-2" href="#">STREAM</v-tab>
       <v-tab class="px-0 pb-2" href="#">SPACES</v-tab>
       <v-tab class="px-0 pb-2" href="#">PEOPLE</v-tab>
+      <v-tab class="px-0 pb-2" href="#">FORUMS</v-tab>
     </v-tabs>
   </v-app>
 </template>
