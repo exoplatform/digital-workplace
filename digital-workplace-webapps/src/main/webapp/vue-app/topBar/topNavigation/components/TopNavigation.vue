009e714f6215253b1d2ccd038e3e9cd3b1c685d0
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2019-11-22 10:34:28 +0100

    US-7.1: Fix navigation topBar display problem in tablet (#89)

--- digital-workplace-webapps/src/main/webapp/vue-app/topBar/topNavigation/components/TopNavigation.vue
+++ digital-workplace-webapps/src/main/webapp/vue-app/topBar/topNavigation/components/TopNavigation.vue
@@ -1,8 +1,7 @@
 <template>
   <v-app 
     id="NavigationToolbarPortlet"
-    class="VuetifyApp"
-    style="max-width: 400px;">
+    class="VuetifyApp">
     <v-tabs center-active color="#578DC9">
       <v-tab class="px-0 pb-2" href="home">SNAPSHOT</v-tab>
       <v-tab class="px-0 pb-2" href="#">STREAM</v-tab>
