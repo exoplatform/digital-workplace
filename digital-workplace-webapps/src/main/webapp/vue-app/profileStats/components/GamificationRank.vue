119aa2cf62af3dbadb552b46d1520d33d4c13f5a
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2020-01-28 17:50:23 +0100

    US-3.3.6: Fix Rank widget issues (#164)

--- digital-workplace-webapps/src/main/webapp/vue-app/profileStats/components/GamificationRank.vue
+++ digital-workplace-webapps/src/main/webapp/vue-app/profileStats/components/GamificationRank.vue
@@ -164,7 +164,11 @@
             for (let i = 0; i < data.length; i++) {
               this.leaderBoardArray.push(data[i])
             }
-            if (data.length < 4) {
+            if (data.length === 6 || index < 7) {
+              for (let i = 3; i < 6; i++) {
+                this.listBelowPoduim.push(data[i])
+              }
+            } else if (data.length < 3) {
               for (let i = 0; i < data.length; i++) {
                 this.listBelowPoduim.push(data[i])
               }
