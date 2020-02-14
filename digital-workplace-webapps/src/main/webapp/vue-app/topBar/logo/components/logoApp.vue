931eba15394ab8ad9917db7589b569da4b159208
Ayoub Zayati <azayati@exoplatform.com>
2020-02-14 11:53:20 +0100

    Make DW logo and label links dynamic by current site
    Signed-off-by: Ayoub Zayati <azayati@exoplatform.com>

--- digital-workplace-webapps/src/main/webapp/vue-app/topBar/logo/components/logoApp.vue
+++ digital-workplace-webapps/src/main/webapp/vue-app/topBar/logo/components/logoApp.vue
@@ -7,15 +7,26 @@
       pa-3
       class="white">
       <v-row class="mx-0">
-        <a href="/portal/dw/home" class=" pr-3 logoContainer">
+        <a :href="homeLink" class=" pr-3 logoContainer">
           <v-img
             src="/digital-workplace/images/logo_exo.png"
             max-width="75"/>
         </a>
-        <a href="/portal/dw/home" class=" pl-2 align-self-center brandingContainer">
+        <a :href="homeLink" class=" pl-2 align-self-center brandingContainer">
           <span class="subtitle-2 font-weight-bold">Digital Workplace</span>
         </a>
       </v-row>
     </v-container>
   </v-app>
 </template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+        homeLink: `${eXo.env.portal.context}/${eXo.env.portal.portalName}`
+      }
+    },
+  }
+</script>
