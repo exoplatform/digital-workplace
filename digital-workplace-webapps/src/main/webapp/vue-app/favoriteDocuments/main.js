060414e86a706a4d809face7a51bc972c5b7f2b3
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2020-04-14 12:21:36 +0100

    US-9: Remove tasks widget (#267)

--- digital-workplace-webapps/src/main/webapp/vue-app/favoriteDocuments/main.js
+++ digital-workplace-webapps/src/main/webapp/vue-app/favoriteDocuments/main.js
@@ -5,25 +5,14 @@ import '../../css/main.less';
 Vue.use(Vuetify);
 
 const vuetify = new Vuetify({
-    dark: true,
-    iconfont: '',
+  dark: true,
+  iconfont: '',
 });
 
-// getting language of user
-const lang = eXo && eXo.env && eXo.env.portal && eXo.env.portal.language || 'en';
-
-const resourceBundleName = 'locale.portlet.dw.Portlets';
-const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/${resourceBundleName}-${lang}.json`;
-
 export function init() {
-//getting locale ressources
-exoi18n.loadLanguageAsync(lang, url)
-    .then(i18n => {
-        // init Vue app when locale ressources are ready
-        new Vue({
-            render: h => h(favoriteDocumentsApp),
-            i18n,
-            vuetify,
-        }).$mount('#digital-workplace-favorite-documents');
-    });
+// init Vue app when locale ressources are ready
+  new Vue({
+    render: h => h(favoriteDocumentsApp),
+    vuetify,
+  }).$mount('#digital-workplace-favorite-documents');
 }
